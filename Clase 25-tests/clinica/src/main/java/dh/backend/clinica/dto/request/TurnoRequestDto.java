6f4842e4b330584006aef30f91d593740d161528
veronicajujuy <veronica.jujuy@gmail.com>
2024-09-05 00:26:56 -0300

    modificando validaciones

--- Clase 25-tests/clinica/src/main/java/dh/backend/clinica/dto/request/TurnoRequestDto.java
+++ Clase 25-tests/clinica/src/main/java/dh/backend/clinica/dto/request/TurnoRequestDto.java
@@ -13,9 +13,9 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class TurnoRequestDto {
-    @NotBlank(message = "El codigo de paciente no debe ser nulo")
+    @NotNull(message = "El codigo de paciente no debe ser nulo")
     private Integer paciente_id;
-    @NotBlank(message = "El codigo de odontologo no debe ser nulo")
+    @NotNull(message = "El codigo de odontologo no debe ser nulo")
     private Integer odontologo_id;
     @NotNull(message = "La fecha no debe estar vacia")
     private String fecha;
